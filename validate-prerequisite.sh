7cf1c03aadbd3154f8b60565fbbd512925862116
cueopsbot-cuelogic <cueops-bot@cuelogic.com>
2018-03-19 19:15:03 +0530

    update prerequisite.sh

--- validate-prerequisite.sh
+++ validate-prerequisite.sh
@@ -14,39 +14,61 @@ echo -e "\n\n"
 echo "------------------------------------------------------------"
 echo "***** Validating if Docker installed *****"
 echo "------------------------------------------------------------"
-docker -v
+
+sudo dpkg -l | grep docker > /dev/null 2>&1
+
 if [ $? -eq 0 ]; then
 	
 	echo -e "${green}\xE2\x9C\x94 ${reset}Docker installed..."
 else
-	echo -e "${red}\xE2\x9D\x8C ${reset}Docker not found..."
+	echo -e "${red}\xE2\x9D\x8C ${reset}Docker not found...Installing Docker.."
+	sudo apt-get update
+    sudo apt-get install -y apt-transport-https 
+    sudo apt-get install -y ca-certificates 
+    sudo apt-get install -y  curl 
+    sudo apt-get install -y software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    echo -e "${green}\xE2\x9C\x94 ${reset}Docker installed..."
 fi
-
-
-#sleep 3
-#echo -e "\n\n"
-#echo "------------------------------------------------------------"
-#echo "***** Validating if Docker Compose installed *****"
-#echo "------------------------------------------------------------"
-#docker-compose -v
-#if [ $? -eq 0 ]; then
-	
-#	echo -e "${green}\xE2\x9C\x94 ${reset}Docker Compose installed..."
-#else
-#	echo -e "${red}\xE2\x9D\x8C ${reset}Docker Compose not found..."
-#fi
-
 
 sleep 3
 echo -e "\n\n"
 echo "------------------------------------------------------------"
-echo "***** Validating operating system limits on mmap counts *****"
+echo "***** Validating if Docker Swarm Certs Generated *****"
 echo "------------------------------------------------------------"
-CURRENT_MAP_COUNT=$(sysctl vm.max_map_count)
-echo "Current operating system limit ${red}${CURRENT_MAP_COUNT}${reset} is likely to be too low, which may result in out of memory exceptions for Elasticsearch.." 
-echo "Increasing the limits to run Elasticsearch container to avoid out of memory exceptions.."
-INCREASED_MAP_COUNT=$(sysctl -w vm.max_map_count=262144)
-echo -e "updated map count limit : ${green}\xE2\x9C\x94 ${INCREASED_MAP_COUNT}${reset}"
 
+if [ -f ca.pem ]; then
+	echo -e "${green}\xE2\x9C\x94 ${reset}Certs Already Generated..!!"
+    else
+	echo "exporting docker over TCP port 2376....!"
+	cd /home/ubuntu
+	export ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4/)
+	openssl genrsa -out ca-key.pem 4096
+	openssl req -new -x509 -nodes -days 365 -subj "/CN=${HOSTNAME}" -key ca-key.pem -sha256 -out ca.pem	
+	openssl genrsa -out server-key.pem 4096
+	openssl req -subj "/CN=${ip}" -sha256 -new -key server-key.pem -out server.csr
+	echo subjectAltName = DNS:${ip},IP:${ip} >> extfile.cnf
+	echo extendedKeyUsage = serverAuth >> extfile.cnf
+	openssl x509 -req -days 365 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem \
+  	-CAcreateserial -out server-cert.pem -extfile extfile.cnf
+	openssl genrsa -out key.pem 4096
+	openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+	echo extendedKeyUsage = clientAuth >> extfile.cnf
+	openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem \
+	 -CAcreateserial -out cert.pem -extfile extfile.cnf
+	sudo rm -v client.csr server.csr
+	chmod -v 0400 ca-key.pem key.pem server-key.pem
+	chmod -v 0444 ca.pem server-cert.pem cert.pem
+        echo -e "${green}\xE2\x9C\x94 ${reset}Docker certs generated..."
+	sudo service docker stop
+	sudo nohup dockerd -H=unix:///var/run/docker.sock --tlsverify --tlscacert=ca.pem --tlscert=server-cert.pem --tlskey=server-key.pem -H=tcp://0.0.0.0:2376 &
+    echo -e "${green}\xE2\x9C\x94 ${reset}Docker daemon exported over TCP 2376 securely..."
+fi 
 
 
