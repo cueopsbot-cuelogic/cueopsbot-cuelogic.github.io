3f858c0be76d9058e9a78bbd2a004ca59fb82ea2
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-03-28 19:01:07 +0530

    Update validate-prerequisite.sh

--- validate-prerequisite.sh
+++ validate-prerequisite.sh
@@ -32,7 +32,7 @@ else
     $(lsb_release -cs) \
     stable"
     sudo apt-get update
-    sudo apt-get install docker-ce=17.12.1~ce-0~ubuntu
+    sudo apt-get install -y docker-ce=17.12.1~ce-0~ubuntu
     echo -e "${green}\xE2\x9C\x94 ${reset}Docker installed..."
 fi
 
